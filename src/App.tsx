f2fcef10f92bef0cd85b8a73e154186aebd659a6
mohammed main <elkamalmohammed@gmail.com>
2025-11-07 15:32:12 +0100

    Fri Nov  7 15:32:12 +01 2025

--- src/App.tsx
+++ src/App.tsx
@@ -225,7 +225,7 @@ function App() {
                 )}
             </AnimatePresence> */}
 
-            <nav className="bg-black fixed w-full z-40">
+            <nav className="fixed w-full z-40">
                 <div className="max-w-7xl mx-auto py-2 px-4 sm:px-6 lg:px-8">
                     <div className="flex items-center justify-between h-16">
                         <Header />
